56c458b524673d9a542ee01f7d1a35b02b1b7ea9
clicktronix <s28.morgan@gmail.com>
2018-10-31 14:16:20 +0600

    Added base config and data files

---
+++ src/namespace.ts
@@ -0,0 +1,63 @@
+export const paramOptionsSymbol = Symbol.for('isParam');
+
+export interface IParam {
+  [paramOptionsSymbol]: IParamOptions;
+}
+
+export interface IParamOptions {
+  isRequired?: boolean;
+}
+
+export function isParamGuard(data: any): data is IParam {
+  return !!data && !!data[paramOptionsSymbol];
+}
+
+export interface IQueryParams {
+  [param: string]: string;
+}
+
+export interface IPathItem {
+  value: string;
+  isParam: boolean;
+}
+
+export interface IRoutable extends IRoutableBase {
+  getRedirectPath: GetPathFunc;
+}
+
+export type GetPathFunc = IGetPath<string, true>['getRedirectPath'] & IGetPath<string, null>['getRedirectPath'];
+
+export interface IRawRouteTree {
+  [routeKey: string]: IRawRouteBranch;
+}
+
+type IRawRouteBranch = IRawRouteTree | IParam | (IRawRouteTree & IParam) | null;
+
+export function isRawRouteTree(value: IRawRouteBranch): value is IRawRouteTree {
+  const isOnlyParam: boolean = isParamGuard(value) && !Object.keys(value).length;
+  const isNull = value === null;
+  return !isNull && !isOnlyParam;
+}
+
+export type RouteTree<T, Acc extends string = never, WithParam extends true | null = null> = {
+  [P in Extract<keyof T, string>]:
+  & RouteTree<T[P], AccumulateParamKey<T[P], Acc, P>, CheckWithParams<T[P], WithParam>>
+  & IGetPath<AccumulateParamKey<T[P], Acc, P>, CheckWithParams<T[P], WithParam>>
+  & IRoutableBase;
+};
+
+type AccumulateParamKey<T, Acc extends string, Cur extends string> =
+  T extends IParam ? Acc | Cur : Acc;
+
+type CheckWithParams<T, Prev extends true | null> =
+  Prev extends true ? true :
+  T extends IParam ? true : null;
+
+type IGetPath<P extends string, WithParam extends true | null> = WithParam extends true
+  ? { getRedirectPath(params: Record<P, string>, queryParams?: Record<string, string>): string }
+  : { getRedirectPath(queryParams?: Record<string, string>): string; };
+
+interface IRoutableBase {
+  getRoutePath(): string;
+  getElementKey(): string;
+}
